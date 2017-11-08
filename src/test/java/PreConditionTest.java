aa1b584d8ba5d69f8e82a24b5bc6e08668e0b62b
RedDabble <redgeeting+oschina@gmail.com>
2017-11-08 10:14:32 +0800

    guava简单用例

---
+++ src/test/java/PreConditionTest.java
@@ -0,0 +1,41 @@
+import com.google.common.base.Preconditions;
+import org.junit.Test;
+
+/**
+ * Created by whh on 2017/8/23.
+ */
+public class PreConditionTest {
+
+
+    // 参数
+    @Test
+    public void test2(){
+        Preconditions.checkArgument(1==1);
+        Preconditions.checkArgument(1==1,"参数校验");
+        Preconditions.checkArgument(false,"始终返回");
+    }
+
+    //状态
+    @Test
+    public void checkStat(){
+        Preconditions.checkState(true,"状态1");
+        Preconditions.checkState(false,"状态2");
+    }
+
+    // 下标
+    @Test
+    public void checkElement(){
+     //   Preconditions.checkElementIndex(-1,2);
+        Preconditions.checkElementIndex(1,2);
+     //   Preconditions.checkElementIndex(2,2,"下标异常");
+        Preconditions.checkElementIndex(2,2);
+    }
+
+    @Test
+    public void check(){
+       // Preconditions.checkPositionIndex(-1,0);
+      //  Preconditions.checkNotNull(null);
+        Preconditions.checkNotNull(null,"{1}","不能空");
+    }
+
+}
---
+++ src/test/java/StringsTest.java
@@ -0,0 +1,35 @@
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by whh on 2017/8/14.
+ */
+@Slf4j
+public class StringsTest {
+
+    @Test
+    public void testStrings(){
+        System.out.println(Strings.isNullOrEmpty(null));
+        System.out.println(Strings.isNullOrEmpty(""));
+        System.out.println(Strings.nullToEmpty(null));
+        System.out.println(Strings.emptyToNull(""));
+        //用字符补全位数
+        System.out.println( Strings.padEnd("123",10,'0'));
+        // 重复字符串
+        System.out.println(Strings.repeat("233",2));
+    }
+
+    @Test
+    public void testLimit(){
+        List l = Lists.newArrayList(1,2,45,6,3,43,9);
+        List m = Lists.newArrayList(1,45,4);
+        m.retainAll(l);
+
+        log.info("m:{}",m);
+
+    }
+}
