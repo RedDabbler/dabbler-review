8d1f72aecf3062e829a75173ace5c63fe9abbacc
RedDabbler <redgeeting+oschina@gmail.com>
2019-09-27 17:30:29 +0800

    'proxy'

---
+++ common-review/src/main/java/com/redDabbler/review/common/jdk/io/RandomAccessFileDemo.java
@@ -0,0 +1,24 @@
+package com.redDabbler.review.common.jdk.io;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileDemo {
+
+
+    public static void main(String[]args)throws Exception{
+
+        constructor();
+    }
+
+
+    private static void constructor()throws FileNotFoundException {
+
+        // 读模式下，文件不存在会报错
+      //  RandomAccessFile randomAccessFile = new RandomAccessFile("E://1.txt","r");
+        //读写模式下，文件不存在会创建一个
+        RandomAccessFile randomAccessFile1 = new RandomAccessFile("E://1.txt","rw");
+        //
+        RandomAccessFile randomAccessFile2 = new RandomAccessFile("E://1.txt","rws");
+    }
+}
---
+++ common-review/src/main/java/com/redDabbler/review/common/jdk/net/ProxyDemo.java
@@ -0,0 +1,60 @@
+package com.redDabbler.review.common.jdk.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class ProxyDemo {
+
+
+    public static void main(String[]args) {
+        String proxyHost = "127.0.0.1";
+        String proxyPort = "1080";
+
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+
+        demo2();
+    }
+
+    public static void demo1() {
+        URL url = null;
+        try {
+            url = new URL("http://www.google.com");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            URLConnection urlConnection = url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static  void demo2(){
+        SocketAddress socketAddress = new InetSocketAddress("127.0.0.1",1080);
+        Proxy proxy = new Proxy(Proxy.Type.HTTP,socketAddress);
+        try {
+            URL url = new URL("http://www.google.com");
+            URLConnection urlConnection = url.openConnection(proxy);
+            InputStream inputStream = urlConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
